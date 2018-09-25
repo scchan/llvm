9d804befafb8c7e974bcd5f0b371a0ba0fe644e9
Siu Chi Chan <siuchi.chan@amd.com>
2018-09-25 12:16:29 -0400

    Squashed commit of the following:
    commit 3ef9e8c20a7cb75879e3a1455f4790196ee333f5
    Author: Siu Chi Chan <siuchi.chan@amd.com>
    Date:   Fri Aug 31 16:43:01 2018 -0400
    
        Speed up SAC when erasing unwanted functions by deleting
        the function body first before other stuffs.
    
        Change-Id: If728dec00e1697949cf386bbb23b05833ca1a7be
    
    commit 5aa303e387c75aadbd9bce39c246b8cf9328e405
    Author: Sameer Sahasrabuddhe <ssahasra@amd.com>
    Date:   Tue Aug 28 13:05:40 2018 +0530
    
        SelectAcceleratorCode: strip pointer casts when examining a call
    
        At a call-site, the callee may not directly be a pointer to the
        function; it can also be a cast expression even that points to the
        actual function. This can happen even without an indirect call. The
        traversal of the call-graph needs to be aware of such pointer casts.
    
        Change-Id: I271cc2543d6ea73fe7f04df25976e656e5314725
    
    commit e57afc1685e9fcb8931c2eee331f32963ddf4e15
    Author: Sameer Sahasrabuddhe <ssahasra@amd.com>
    Date:   Wed Aug 22 15:11:16 2018 +0530
    
        do not traverse callgraph during pass initialization
    
        The initializer for SelectAcceleratorCode traverses the call-graph to
        create a list of functions that are called on the device side. But
        this stage is too early, since other passes can change the call-graph
        before the pass actually runs.
    
        Hence moving the traversal to runOnModule().
    
        Change-Id: Id0f97382b7130d8e95295d7c5fbc5936ea002b16
    
    Change-Id: If271255420e14c0f5fd7dd921c556254ca6644e7

--- lib/Transforms/HC/SelectAcceleratorCode/SelectAcceleratorCode.cpp
+++ lib/Transforms/HC/SelectAcceleratorCode/SelectAcceleratorCode.cpp
@@ -38,10 +38,10 @@ class SelectAcceleratorCode : public ModulePass {
     {
         for (auto&& BB : F) {
             for (auto&& I : BB) {
-                if (I.getOpcode() == Instruction::Call) {
-                    auto Callee = cast<CallInst>(I).getCalledFunction();
-                    if (Callee) {
-                        auto Tmp = HCCallees_.insert(M.getFunction(Callee->getName()));
+                if (auto CI = dyn_cast<CallInst>(&I)) {
+                    auto V = CI->getCalledValue()->stripPointerCasts();
+                    if (auto Callee = dyn_cast<Function>(V)) {
+                        auto Tmp = HCCallees_.insert(Callee);
                         if (Tmp.second) findAllHCCallees_(*Callee, M);
                     }
                 }
@@ -77,14 +77,16 @@ class SelectAcceleratorCode : public ModulePass {
         return erasedSome;
     }
 
-    bool eraseNonHCFunctions_(Module &M) const
+    bool eraseNonHCFunctionsBody_(Module &M) const
     {
-        return eraseIf_(
-            [&]() { return M.begin(); },
-            [&]() { return M.end(); },
-            [&, this](const Function &F) {
-                return HCCallees_.count(M.getFunction(F.getName())) == 0;
-            });
+        bool Modified = false;
+        for (auto&& F : M.functions()) {
+          if (HCCallees_.count(M.getFunction(F.getName())) == 0) {
+            F.deleteBody();
+            Modified = true;
+          }
+        };
+        return Modified;
     }
 
     bool eraseDeadGlobals_(Module &M) const
@@ -121,8 +123,11 @@ public:
     static char ID;
     SelectAcceleratorCode() : ModulePass{ID} {}
 
-    bool doInitialization(Module &M) override
-    {   // TODO: this may represent a valid analysis pass.
+    bool doInitialization(Module &M) override { return false; }
+
+    bool runOnModule(Module &M) override {
+        // This may be a candidate for an analysis pass that is
+        // invalidated appropriately by other passes.
         for (auto&& F : M.functions()) {
             if (F.getCallingConv() == CallingConv::AMDGPU_KERNEL) {
                  auto Tmp = HCCallees_.insert(M.getFunction(F.getName()));
@@ -130,12 +135,7 @@ public:
             }
         }
 
-        return false;
-    }
-
-    bool runOnModule(Module &M) override
-    {
-        bool Modified = eraseNonHCFunctions_(M);
+        bool Modified = eraseNonHCFunctionsBody_(M);
 
         Modified = eraseDeadGlobals_(M) || Modified;
 
